3c8f97810c1f173e8a22f7a0e8576b73d10f59a2
Gregor <i.love.plik@gmail.com>
2022-01-30 16:53:05 +0100

    'initial'

---
+++ src/main/java/com/gkieszek/cryptocurrencyData/CryptocurrencyDataApplication.java
@@ -0,0 +1,13 @@
+package com.gkieszek.cryptocurrencyData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyDataApplication.class, args);
+	}
+
+}
