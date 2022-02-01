908a22901f915319499d03fb77b5f374de66aa12
Gregor <i.love.plik@gmail.com>
2022-02-01 19:09:44 +0100

    add class dto

--- src/main/java/com/gkieszek/cryptocurrencyData/controller/CryptoController.java
+++ src/main/java/com/gkieszek/cryptocurrencyData/controller/CryptoController.java
@@ -1,5 +1,6 @@
 package com.gkieszek.cryptocurrencyData.controller;
 
+import com.gkieszek.cryptocurrencyData.dto.CryptoInfo;
 import com.gkieszek.cryptocurrencyData.dto.Currency;
 import com.gkieszek.cryptocurrencyData.service.CryptoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,14 @@ public class CryptoController {
     @Autowired
     CryptoService cryptoService;
 
-    @GetMapping("/cryptocurrency/{id}{vs}")
-    public Currency getCurrency(@PathVariable String id, @PathVariable String vs){
-        return cryptoService.getCurrency(id, vs);
+//    @GetMapping("/cryptocurrency/{id}/{vs}")
+//    public Currency getCurrency(@PathVariable String id, @PathVariable String vs){
+//        return cryptoService.getCurrency(id, vs);
+//    }
 
+    @GetMapping("/cryptocurrency/info/{id}")
+    public CryptoInfo getInfo(@PathVariable String id){
+        return cryptoService.getInfoCryptocurrency(id);
     }
-
 
 }
